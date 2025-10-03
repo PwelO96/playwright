37de0d2f2988339c6b6ea4befa3025ad755e0a72
PwelO96 <pawel.ogryzek96@wp.pl>
2025-10-03 15:16:10 +0200

    new test

--- tests/pulpit_tests.spec.ts
+++ tests/pulpit_tests.spec.ts
@@ -40,4 +40,23 @@ test.describe("Pulpit tests", () => {
       "Doładowanie wykonane"
     );
   });
+
+  test.only("correct balance after successful mobile top-up", async ({
+    page,
+  }) => {
+    const receiverOption = "500 xxx xxx";
+    const amount = "50";
+    const initialBalance = await page.locator("#money_value").innerText();
+    const expectedBalance = Number(initialBalance) - Number(amount);
+
+    await page.locator("#widget_1_topup_receiver").selectOption(receiverOption);
+    await page.locator(".number").nth(1).fill(amount);
+    await page.locator("#uniform-widget_1_topup_agreement span").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+    await page.getByTestId("close-button").click();
+
+    await expect(page.locator("#money_value")).toContainText(
+      `${expectedBalance}`
+    );
+  });
 });
